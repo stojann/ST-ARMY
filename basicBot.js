8d58590742bb5a3b282fe8a9fa272f69781acb54
stojann <stojansudzukovic5@gmail.com>
2015-01-05 20:41:31 +0100

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -53,7 +53,7 @@
     var loadChat = function (cb) {
         if (!cb) cb = function () {
         };
-        $.get("https://rawgit.com/Yemasthui/basicBot/master/lang/langIndex.json", function (json) {
+        $.get("https://raw.githubusercontent.com/stojann/ST-ARMY/master/lang/langIndex.json", function (json) {
             var link = basicBot.chatLink;
             if (json !== null && typeof json !== "undefined") {
                 langIndex = json;
